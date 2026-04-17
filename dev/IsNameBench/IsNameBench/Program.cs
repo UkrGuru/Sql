c3760f5e2d467b00ab5cdc57929c3d6fa61d5f6b
A V <alexander@businessnetsolutions.co.uk>
2026-04-17 10:13:55 +0300

    2026-04-17

--- dev/IsNameBench/IsNameBench/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        BenchmarkRunner.Run<IsNameBenchmarks>();
-    }
-}
--- dev/ResultsBench/ResultsBench/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        BenchmarkRunner.Run<BenchParse>();
-    }
-}
